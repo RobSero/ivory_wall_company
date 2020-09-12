fff3b2f56b9dac16cc3a6a97c51ac9a2ecdb3644
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-09-12 11:57:10 +0100

    :fire: NEW - URL PATHS AND VIEWS SETUP

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
+    path('about/', views.aboutpage),
+    path('contact/', views.contactpage),
+    path('portfolio/', views.portfoliopage),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(req):
   return render(req, 'pages/home.html')
+
+
+def aboutpage(req):
+  return render(req, 'pages/about.html')
+
+
+def contactpage(req):
+  return render(req, 'pages/contact.html')
+
+
+def portfoliopage(req):
+  return render(req, 'pages/portfolio.html')
